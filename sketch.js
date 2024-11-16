5fc538d74659b73e12c5abdf1e5939074394ab3f
Abraham Vijai <abrahamvijai03@gmail.com>
2024-11-16 06:35:53 -0500

    Created the starfield

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+const NOISE_SEED = 100;
+const RANDOM_SEED = 100;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700, WEBGL);
 }
 
 function draw() {
-  background(220);
+  background('black');
+
+  // Create the starfield
+  createStarfield(1000)
+
+
+  
+}
+
+function createStarfield(numStars) {
+  noLoop();
+  
+  for (let i = 0; i < numStars; i++) {
+    // Create starfield
+    strokeWeight(3);
+    stroke('white')
+    point(random(-width,width),random(-height,height))
+  }
 }
