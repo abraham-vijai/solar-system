f0edc0874e828c46d2da2cb56759b70027785f0e
Abraham Vijai <abrahamvijai03@gmail.com>
2024-11-22 01:07:29 -0500

    Moved event handlers to ButtonAction.js

---
+++ ButtonAction.js
@@ -0,0 +1,51 @@
+class ButtonAction {
+    constructor(parameters) {
+
+    }
+
+    static removeLastPlanet() {
+        if (planetArray.length > 0) {
+            planetArray.pop(); // Remove the last planet in the array
+        }
+    }
+
+    static updateStarCount() {
+        starPositions = [];
+        starCount = UI_starCount.value();
+    }
+    static resetSystem(params) {
+        planetArray = [];
+    }
+
+    static pauseResume() {
+        if (isPaused) {
+            loop();
+            isPaused = false;
+        }
+        else {
+            noLoop();
+            isPaused = true;
+        }
+    }
+
+    static addPlanet() {
+        let newPlanet;
+
+        // Convert to RGB
+        let hexColor = UI_planetColor.value(); // Get the color from the color picker
+        let c = color(hexColor); // Convert hex to a p5.js color object
+        let rgbColor = [red(c), green(c), blue(c)]; // Extract RGB values as an array
+
+        // Store the new planet in the array
+        planetArray.push(newPlanet = new SpaceObject(
+            UI_planetSize.value(),
+            UI_planetRotationSpeed.value(),
+            15,
+            null,
+            UI_planetSunDistance.value(),
+            UI_planetHasMoon.checked(),
+            rgbColor
+        ));
+    }
+
+}
